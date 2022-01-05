9c97743e212ab538272f293b3adafa1a377a2724
elifilkay <93220459+elifilkay@users.noreply.github.com>
2022-01-05 14:27:17 +0300

    palindrom sayıysa true değilse false yazdır.

---
+++ palindrom.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class palindrom {
+    static boolean isPalindrom(int number){
+         int temp=number;
+         int reverseNumber=0;
+         int lastNumber;
+        while(temp!=0){
+            lastNumber=temp%10;
+            reverseNumber=(reverseNumber*10)+lastNumber;
+            temp/=10;
+
+        }
+        if(number==reverseNumber)
+            return true;
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(2345));
+    }
+}
