9408f570ccaa57d3ea0a02f97719957395a3deca
elifilkay <93220459+elifilkay@users.noreply.github.com>
2022-02-11 17:59:09 +0300

    Add files via upload

---
+++ sayıSıralama.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class sayıSıralama {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Birinci sayıyı giriniz: ");
+        int a= scanner.nextInt();
+        System.out.println("İkinci sayıyı giriniz: ");
+        int b= scanner.nextInt();
+        System.out.println("Üçüncü sayı giriniz: ");
+        int c= scanner.nextInt();
+        if (a<b && a<c){
+            if(b<c){
+                System.out.println("a<b<c");
+            }
+            else{
+                System.out.println("a<c<b");
+            }
+        }
+        else if(b<a && b<c){
+            if (a<c){
+                System.out.println("b<a<c");
+            }
+            else{
+                System.out.println("b<c<a");
+            }
+
+            }
+
+        else if (c<a &&c<b){
+            if (a<b){
+                System.out.println("c<a<b");
+            }
+            else{
+                System.out.println("c<b<a");
+            }
+
+        }
+    }
+}
